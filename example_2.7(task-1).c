109b43f6a42c81fcaba7a83b29e32d2e984619e0
Sohrab Hossain Manik <maniksohrabhossain@gmail.com>
2026-09-15 22:18:30 +0600

    Create example_2.7(task-1).c

---
+++ example_2.7(task-1).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+ {
+  int student_id;
+  int age;
+  char section;
+  float cgpa;
+
+  printf("Enter the value of student Id ");
+  scanf("%d",&student_id);
+
+  printf("Enter the value of age ");
+  scanf("%d",age);
+
+  printf("Enter the value of Section ");
+  scanf(" %c",&section);
+
+  printf("Enter the value of cgpa ");
+  scanf("%f",&cgpa);
+
+  printf("Student Id is %d\n",student_id);
+  printf("Age is %d\n",age);
+  printf("Section is %c\n",section);
+  printf("CGPA is %.2f",cgpa);
+
+return 0;
+}
